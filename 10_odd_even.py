4fd2bdf052c84d674fdbaecb6816b4e633873bbd
vrittech-manoj <manoj@vrittechnologies.com>
2024-12-04 17:47:56 +0545

    odd even

---
+++ 10_odd_even.py
@@ -0,0 +1,4 @@
+even number  ?
+-->if any number is divided by 2 and remainder is 0 then that number is called even number 
+other wise call odd number 
+#WAP to find user input number is odd or even ?
--- 7_condition.py
+++ 7_condition.py
@@ -24,18 +24,28 @@
 # else:
 #     print("you can not vote")
 
-#class work
-age = 6 #take user input
-# WAP to output like {child,adult....old} 
-if isinstance(age,str) and age.isdigit() == False: # if not age.isdigit():
-    print("invalid age")
-elif age>0 and age<13:
-    print("you are child")
-elif age>=13 and age<20:
-    print("you are teenagers")
-elif age<0 or age>200: #True or False =  True
-    print("wrong input")
-else:
-    print("either you are adult or old")
+# #class work
+# age = 6 #take user input
+# # WAP to output like {child,adult....old} 
+# if isinstance(age,str) and age.isdigit() == False: # if not age.isdigit():
+#     print("invalid age")
+# elif age>0 and age<13:
+#     print("you are child")
+# elif age>=13 and age<20:
+#     print("you are teenagers")
+# else: 
+#     print("wrong input")
 
-    
+
+
+mark = int(input("Enter mark percentage#>>>"))
+
+if mark<30: #15 <30 ?
+    print("Fail")
+elif mark>=30 and mark<40: #True and False
+    print("third division")
+
+
+
+
+
