fc0b51d94aa4a23d9f6a31132662dca77bc9d012
Manoj Das <manoj@vrittechnologies.com>
2025-01-02 17:54:37 +0545

    socket

--- socket/client.py
+++ socket/client.py
@@ -4,7 +4,7 @@ s = socket.socket()
 
 port = 12345 
 
-ip = "127.0.0.1"
+ip = "server ip"
 
 s.connect((ip,port))
 print("connected to server")
@@ -12,3 +12,5 @@ print("connected to server")
 s.send(b"Hello server , i am client")
 
 s.close()
+
+input("pause")
--- socket/server.py
+++ socket/server.py
@@ -3,7 +3,7 @@ import socket
 s = socket.socket()
 port = 12345
 
-s.bind(('',port))
+s.bind(('192.168.1.150',port))
 
 print("i am server in listening mode")
 s.listen(5)
@@ -11,6 +11,9 @@ s.listen(5)
 connection, addr = s.accept()
 print("connection from ", addr)
 
-print(connection.recv(1024))
+message = connection.recv(1024)
+print(message)
 
 connection.close()
+
+input("pause")
